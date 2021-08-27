40bb55a6c22c072892c8c6cee3071af984ea4fc4
Isaac Wallace <iwallace96@gmail.com>
2021-08-27 00:28:06 -0400

    complete

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,29 +1,53 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const app = express();
 
+const axios = require('axios');
+const ExpressError = require('./expressError');
 
+app.use(express.json());
 
-app.get('/', function(req, res, next) {
-  console.log(req)
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-    
-
-    return res.send(JSON.stringify(out));
-  } catch(err) {
-    console.log(err)
-    next();
+app.post('/', function(req, res) {
+  if (!req.body.developers) throw new ExpressError('Developer username required', 400);
+    const devInfo = req.body.developers.map(async (dev) => {
+      try {
+      const res = await axios.get(`https://api.github.com/users/${dev}`);
+      if (res.status !== 404) {
+        return {
+          bio : res.data.bio,
+          name: res.data.name,
+        };
+    }
+  } catch {
+    return {message: "There is no such user!"};
   }
 });
- 
-app.get('/'), function(req,res){
-  res.send("yay")
-}
+  Promise.all(devInfo).then((data) => {
+    return res.status(200).json(data);
+  })
+});
 
-app.listen(3000, function(){
-  console.log("app starting on port 3000")
-})
+
+// 404 handler
+app.use(function (req, res, next) {
+  const notFoundError = new ExpressError("Not Found", 404);
+  return next(notFoundError)
+});
+
+// generic error handler
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  let message = err.message;
+// set the status and alert the user
+  return res.status(status).json({
+    error: {message, status}
+  });
+});
+
+// end generic handler
+app.listen(3000, function (err) {
+  if (err) {
+    console.log(`Error starting server: ${err}`);
+  } else {
+    console.log(`Server starting on port 3000`);
+  }
+});
